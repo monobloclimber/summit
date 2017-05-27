e2c2e392c96d855996025411d2ec5e31a5834d11
monobloclimber <poujolclement@gmail.com>
2017-05-27 16:54:16 +0200

    Added "appends" function to attach parameters to the paged url

--- core/pagination/Pagination.php
+++ core/pagination/Pagination.php
@@ -13,6 +13,7 @@ class Pagination {
 	public $total;
 	public $pageNumber;
 	public $currentPage = 1;
+	public $params = "?";
 
 	public function __construct($total, $itemNumber){
 		$this->total      = $total;
@@ -35,6 +36,17 @@ class Pagination {
 		}
 
 		return ['skip' => $skip, 'take' => $take];
+	}
+
+	public function appends($params){
+
+		foreach ($params as $key => $value) {
+			$tab[] = $key.'='.$value;
+		}
+
+		$this->params = '?'.implode('&', $tab).'&';
+
+		return $this;
 	}
 
 	/**
@@ -65,7 +77,7 @@ class Pagination {
 		# Generates previous page
 		$pagination .= '<li class="'.$previous.'">';
 		if($this->currentPage > 1){
-			$pagination .= '<a href="?page='.($this->currentPage - 1).'" aria-label="Previous">';
+			$pagination .= '<a href="'.$this->params.'page='.($this->currentPage - 1).'" aria-label="Previous">';
 		}
 		$pagination .= '<span aria-hidden="true">&laquo;</span>';
 		if($this->currentPage > 1){
@@ -77,38 +89,38 @@ class Pagination {
 		if($this->pageNumber < 5){
 			for($i = 1; $i <= $this->pageNumber; $i++){
 				if($i != $this->currentPage){
-					$pagination .= '<li><a href="?page='.$i.'">'.$i.'</a></li>';
+					$pagination .= '<li><a href="'.$this->params.'page='.$i.'">'.$i.'</a></li>';
 				}else{
-					$pagination .= '<li class="active"><a href="?page='.$i.'">'.$i.'<span class="sr-only">(current)</span></a></li>';
+					$pagination .= '<li class="active"><a href="'.$this->params.'page='.$i.'">'.$i.'<span class="sr-only">(current)</span></a></li>';
 				}
 			}
 		}else{
 			if($this->currentPage < ($this->pageNumber - 1)){
-				$pagination .= '<li class="active"><a href="?page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
-				$pagination .= '<li><a href="?page='.($this->currentPage + 1).'">'.($this->currentPage + 1).'</a></li>';
+				$pagination .= '<li class="active"><a href="'.$this->params.'page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page='.($this->currentPage + 1).'">'.($this->currentPage + 1).'</a></li>';
 			}else{
-				$pagination .= '<li><a href="?page=1">1</a></li>';
-				$pagination .= '<li><a href="?page=2">2</a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page=1">1</a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page=2">2</a></li>';
 			}
 
 			$pagination .= '<li class="disabled"><span>...</span></li>';
 
 			if($this->currentPage < ($this->pageNumber - 1)){
-				$pagination .= '<li><a href="?page='.($this->pageNumber - 1).'">'.($this->pageNumber - 1).'</a></li>';
-				$pagination .= '<li><a href="?page='.$this->pageNumber.'">'.$this->pageNumber.'</a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page='.($this->pageNumber - 1).'">'.($this->pageNumber - 1).'</a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page='.$this->pageNumber.'">'.$this->pageNumber.'</a></li>';
 			}elseif($this->currentPage == ($this->pageNumber - 1)){
-				$pagination .= '<li class="active"><a href="?page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
-				$pagination .= '<li><a href="?page='.$this->pageNumber.'">'.$this->pageNumber.'</a></li>';
+				$pagination .= '<li class="active"><a href="'.$this->params.'page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page='.$this->pageNumber.'">'.$this->pageNumber.'</a></li>';
 			}else{
-				$pagination .= '<li><a href="?page='.($this->pageNumber - 1).'">'.($this->pageNumber - 1).'</a></li>';
-				$pagination .= '<li class="active"><a href="?page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
+				$pagination .= '<li><a href="'.$this->params.'page='.($this->pageNumber - 1).'">'.($this->pageNumber - 1).'</a></li>';
+				$pagination .= '<li class="active"><a href="'.$this->params.'page='.$this->currentPage.'">'.$this->currentPage.'<span class="sr-only">(current)</span></a></li>';
 			}
 		}
 
 		# Generates next page
 		$pagination .= '<li class="'.$next.'">';
 		if($this->currentPage != $this->pageNumber){
-			$pagination .= '<a href="?page='.($this->currentPage + 1).'" aria-label="Next">';
+			$pagination .= '<a href="'.$this->params.'page='.($this->currentPage + 1).'" aria-label="Next">';
 		}
 		$pagination .= '<span aria-hidden="true">&raquo;</span>';
 		if($this->currentPage != $this->pageNumber){
